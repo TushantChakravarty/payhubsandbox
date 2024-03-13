048e535cd4dd48eeded038b005fee3f26c869470
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2024-03-13 16:23:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,57 +7,82 @@ require("dotenv").config();
 var res = require("dotenv").config();
 const cron = require("node-cron");
 const config = require("./lib/config");
-const { myFunction, updateAdmin } = require("./lib/user/scheduler/scheduler");
+const {
+  myFunction,
+  updateAdmin,
+  updateAdminYesterdayTx,
+} = require("./lib/user/scheduler/scheduler");
 const adminDao = require("./lib/user/adminDao");
-const { getTransactionsSummaryYesterday } = require("./lib/user/transactionsDao/TransactionDao");
-const { MongoClient } = require('mongodb');
+const {
+  getTransactionsSummaryYesterday,
+} = require("./lib/user/transactionsDao/TransactionDao");
+const { MongoClient } = require("mongodb");
 const { getAllUsers } = require("./lib/user/userDao");
+const {
+  updatePendingTransactionStatus,
+} = require("./lib/user/scheduler/statusScheduler");
+const {
+  updateVolumeDataPayouts,
+  getTotalAdminVolumePayouts,
+  updatePayoutsBalanceMerchants,
+} = require("./lib/user/payouts/payoutsDao");
+// const { getAllPendinTransactionsPaythrough } = require("./lib/controllers/paythrough");
+// const { updatePendingTransactionStatus } = require("./lib/user/scheduler/statusScheduler");
 
+cron.schedule("0 30 18 * * *", async () => {
+  // Get the last execution date from the file
+  const admin = await adminDao.getUserDetails({
+    emailId: "samir123@payhub",
+  });
+  const lastExecutionDate = admin.lastExecutionDate;
 
+  // Get the current date
+  const currentDate = new Date().toISOString().split("T")[0];
 
-// cron.schedule("0 30 18 * * *", async () => {
-//   // Get the last execution date from the file
-//   const admin = await adminDao.getUserDetails({
-//     emailId: "samir123@payhub",
-//   });
-//   const lastExecutionDate = admin.lastExecutionDate;
+  // Check if the function has not been executed today
+  if (lastExecutionDate !== currentDate) {
+    // Run your function
+    myFunction();
+    updateAdmin();
+    updateAdminYesterdayTx();
+    console.log("running");
 
-//   // Get the current date
-//   const currentDate = new Date().toISOString().split("T")[0];
+    // Update the last execution date in the file
+    //fs.writeFileSync(DATE_FILE, currentDate);
+    const update = {
+      lastExecutionDate: currentDate,
+    };
+    adminDao.updateProfile(
+      {
+        emailId: "samir123@payhub",
+      },
+      update
+    );
+  }
+});
+cron.schedule("0 40 18 * * *", async () => {
+  updateAdminYesterdayTx();
+});
+//updateAdminYesterdayTx()
+cron.schedule("*/10 * * * *", async () => {
+  await adminDao.updateVolumeData("success");
+  await adminDao.getTotalVolume("success");
+  await adminDao.updateGatewayVolumeData();
+await updateVolumeDataPayouts("success")
+await getTotalAdminVolumePayouts("success")
+});
 
-//   // Check if the function has not been executed today
-//   if (lastExecutionDate !== currentDate) {
-//     // Run your function
-//     myFunction();
-//     console.log("running");
-
-//     // Update the last execution date in the file
-//     //fs.writeFileSync(DATE_FILE, currentDate);
-//     const update = {
-//       lastExecutionDate: currentDate,
-//     };
-//     adminDao.updateProfile(
-//       {
-//         emailId: "samir123@payhub",
-//       },
-//       update
-//     );
-//   }
+cron.schedule("*/30 * * * *", async () => {
+  await adminDao.updateTotalGatewayBalance();
+  await adminDao.updateBalanceMerchants()
+  await adminDao.updateBalanceAdmin()
+});
+//updatePayoutsBalanceMerchants()
+// cron.schedule("*/40 * * * *", async () => {
+// updatePendingTransactionStatus()
 // });
 
-// cron.schedule("*/60 * * * *", async () => {
-//   await adminDao.updateVolumeData("success");
-//   await adminDao.getTotalVolume("success");
-//   await adminDao.updateGatewayVolumeData();
-// });
-
-
-// cron.schedule("*/30 * * * *", async () => {
-//   await adminDao.updateTotalGatewayBalance();
-//   await adminDao.updateBalanceMerchants()
-//   await adminDao.updateBalanceAdmin()
-// });
-
+//getTransactionsSummaryYesterday()
 
 config.dbConfig((err) => {
   if (err) {
@@ -72,7 +97,7 @@ config.dbConfig((err) => {
 
   // init express app
   const app = express();
-  app.set('trust proxy', true)
+  app.set("trust proxy", true);
 
   // config express
   config.expressConfig(app);

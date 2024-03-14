a0ac2e3860739f1f6599676b69424a0d10e5d5d9
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2024-03-14 14:05:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -64,19 +64,19 @@ cron.schedule("0 40 18 * * *", async () => {
   updateAdminYesterdayTx();
 });
 //updateAdminYesterdayTx()
-cron.schedule("*/10 * * * *", async () => {
-  await adminDao.updateVolumeData("success");
-  await adminDao.getTotalVolume("success");
-  await adminDao.updateGatewayVolumeData();
-await updateVolumeDataPayouts("success")
-await getTotalAdminVolumePayouts("success")
-});
+// cron.schedule("*/10 * * * *", async () => {
+//   await adminDao.updateVolumeData("success");
+//   await adminDao.getTotalVolume("success");
+//   await adminDao.updateGatewayVolumeData();
+// await updateVolumeDataPayouts("success")
+// await getTotalAdminVolumePayouts("success")
+// });
 
-cron.schedule("*/30 * * * *", async () => {
-  await adminDao.updateTotalGatewayBalance();
-  await adminDao.updateBalanceMerchants()
-  await adminDao.updateBalanceAdmin()
-});
+// cron.schedule("*/30 * * * *", async () => {
+//   await adminDao.updateTotalGatewayBalance();
+//   await adminDao.updateBalanceMerchants()
+//   await adminDao.updateBalanceAdmin()
+// });
 //updatePayoutsBalanceMerchants()
 // cron.schedule("*/40 * * * *", async () => {
 // updatePendingTransactionStatus()

07a97ab1f37104dd904b7f9c985f7d64c8792b30
TushantChakravarty <86719728+TushantChakravarty@users.noreply.github.com>
2024-09-11 11:03:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,54 +29,57 @@ const {
 // const { getAllPendinTransactionsPaythrough } = require("./lib/controllers/paythrough");
 // const { updatePendingTransactionStatus } = require("./lib/user/scheduler/statusScheduler");
 
-// cron.schedule("0 30 18 * * *", async () => {
-//   // Get the last execution date from the file
-//   const admin = await adminDao.getUserDetails({
-//     emailId: "samir123@payhub",
-//   });
-//   const lastExecutionDate = admin.lastExecutionDate;
+cron.schedule("0 30 18 * * *", async () => {
+  // Get the last execution date from the file
+  const admin = await adminDao.getUserDetails({
+    emailId: "samir123@payhub",
+  });
+  const lastExecutionDate = admin.lastExecutionDate;
 
-//   // Get the current date
-//   const currentDate = new Date().toISOString().split("T")[0];
+  // Get the current date
+  const currentDate = new Date().toISOString().split("T")[0];
 
-//   // Check if the function has not been executed today
-//   if (lastExecutionDate !== currentDate) {
-//     // Run your function
-//     myFunction();
-//     updateAdmin();
-//     updateAdminYesterdayTx();
-//     console.log("running");
+  // Check if the function has not been executed today
+  if (lastExecutionDate !== currentDate) {
+    // Run your function
+    myFunction();
+    updateAdmin();
+    updateAdminYesterdayTx();
+    console.log("running");
 
-//     // Update the last execution date in the file
-//     //fs.writeFileSync(DATE_FILE, currentDate);
-//     const update = {
-//       lastExecutionDate: currentDate,
-//     };
-//     adminDao.updateProfile(
-//       {
-//         emailId: "samir123@payhub",
-//       },
-//       update
-//     );
-//   }
-// });
-// cron.schedule("0 40 18 * * *", async () => {
-//   updateAdminYesterdayTx();
-// });
-// //updateAdminYesterdayTx()
-// cron.schedule("10 */2 * * *", async () => {
-//   await adminDao.updateVolumeData("success");
-//   await adminDao.getTotalVolume("success");
-//   await adminDao.updateGatewayVolumeData();
-//   await updateVolumeDataPayouts("success");
-//   await getTotalAdminVolumePayouts("success");
-// });
+    // Update the last execution date in the file
+    //fs.writeFileSync(DATE_FILE, currentDate);
+    const update = {
+      lastExecutionDate: currentDate,
+    };
+    adminDao.updateProfile(
+      {
+        emailId: "samir123@payhub",
+      },
+      update
+    );
+  }
+});
+cron.schedule("0 40 18 * * *", async () => {
+  updateAdminYesterdayTx();
+});
+//updateAdminYesterdayTx()
+// Runs every 5 hours
+cron.schedule("0 */5 * * *", async () => {
+  await adminDao.updateVolumeData("success");
+  await adminDao.getTotalVolume("success");
+  await adminDao.updateGatewayVolumeData();
+  await updateVolumeDataPayouts("success");
+  await getTotalAdminVolumePayouts("success");
+});
 
-// cron.schedule("30 */2 * * *", async () => {
-//   await adminDao.updateTotalGatewayBalance();
-//   await adminDao.updateBalanceMerchants();
-//   await adminDao.updateBalanceAdmin();
-// });
+// Runs every 6 hours
+cron.schedule("0 */6 * * *", async () => {
+  await adminDao.updateTotalGatewayBalance();
+  await adminDao.updateBalanceMerchants();
+  await adminDao.updateBalanceAdmin();
+});
+
 //updatePayoutsBalanceMerchants()
 // cron.schedule("40 */2 * * *", async () => {
 //   updatePendingTransactionStatus();
